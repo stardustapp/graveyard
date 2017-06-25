3295357a2f18ab7d0a7ec02228a66c77a0d0b972
Daniel Lamando <apt@danopia.net>
2017-06-24 22:45:09 -0700

    add omitempty to nsapi json structs

--- core/drivers/nsapi.go
+++ core/drivers/nsapi.go
@@ -2,23 +2,23 @@ package drivers
 
 type nsRequest struct {
 	Op    string
-	Path  string
-	Dest  string // destination for output
-	Input *nsEntry
+	Path  string `json:",omitempty"`
+	Dest  string `json:",omitempty"` // destination for output
+	Input *nsEntry `json:",omitempty"`
 }
 
 type nsResponse struct {
 	Ok     bool
-	Output *nsEntry
+	Output *nsEntry `json:",omitempty"`
 }
 
 type nsEntry struct {
 	Name string
-	Type string
+	Type string `json:",omitempty"`
 
-	StringValue string
-	FileData    []byte
-	Children    []nsEntry
+	StringValue string `json:",omitempty"`
+	FileData    []byte `json:",omitempty"`
+	Children    []nsEntry `json:",omitempty"`
 }
 
 /*
