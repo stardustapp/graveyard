85c4934a10b9e15ac847c14d82553092c7418edc
Daniel Lamando <apt@danopia.net>
2017-07-03 22:23:09 -0700

    skychart: Only return metadata that is present

--- core/utils/skychart/chart.go
+++ core/utils/skychart/chart.go
@@ -12,25 +12,32 @@ type Chart struct {
 }
 
 func (c *Chart) getEntry() base.Entry {
-	ownerName, _ := c.ctx.GetString("/owner-name")
-	ownerEmail, _ := c.ctx.GetString("/owner-email")
-	createdDate, _ := c.ctx.GetString("/created-date")
-	homeDomain, _ := c.ctx.GetString("/home-domain")
 	entriesDir, ok := c.ctx.GetFolder("/entries")
 	if !ok {
 		return nil
 	}
 
-	return inmem.NewFolderOf("chart",
-		ownerName,
-		ownerEmail,
-		createdDate,
-		homeDomain,
+	ent := inmem.NewFolderOf("chart",
 		inmem.NewFolderOf("manage",
 			&chartManageFunc{c, entriesDir},
 		).Freeze(),
 		inmem.NewFolderOf("browse",
 			&chartBrowseFunc{c, entriesDir},
 		).Freeze(),
-	).Freeze()
+	)
+
+	if ownerName, ok := c.ctx.GetString("/owner-name"); ok {
+		ent.Put(ownerName.Name(), ownerName)
+	}
+	if ownerEmail, ok := c.ctx.GetString("/owner-email"); ok {
+		ent.Put(ownerEmail.Name(), ownerEmail)
+	}
+	if createdDate, ok := c.ctx.GetString("/created-date"); ok {
+		ent.Put(createdDate.Name(), createdDate)
+	}
+	if homeDomain, ok := c.ctx.GetString("/home-domain"); ok {
+		ent.Put(homeDomain.Name(), homeDomain)
+	}
+
+	return ent.Freeze()
 }
