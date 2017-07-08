0a6cca1814dbcd21a4fbc28df354604903684b40
Daniel Lamando <apt@danopia.net>
2017-07-08 16:01:13 -0700

    skychart: Create virtual import nodes

--- core/utils/skychart/dag/compile.go
+++ core/utils/skychart/dag/compile.go
@@ -1,6 +1,9 @@
 package dag
 
 import (
+	"fmt"
+	"log"
+	"net/url"
 	"strings"
 
 	"github.com/stardustapp/core/extras"
@@ -8,33 +11,63 @@ import (
 
 // TODO: automatically build multibind mounts?
 
-func (graph *Graph) Compile() bool {
+func (g *Graph) getImportNode(uri string) *Node {
+	// check if the import already exists
+	for _, node := range g.nodes {
+		if node.nodeType == "Import" && node.deviceUri == uri {
+			return node
+		}
+	}
+
+	// create new import node
+	node := &Node{
+		id:         extras.GenerateId(),
+		nodeType:   "Import",
+		mountPath:  "/mnt/" + strings.Replace(uri, "/", "-", -1),
+		deviceType: "ActiveMount",
+		deviceUri:  uri,
+	}
+	g.nodes[node.id] = node
+	return node
+}
+
+func (g *Graph) Compile() bool {
 	// Graphs start with a virtual root node
-	graph.nodes["root"] = &Node{
-		id:        "root",
-		nodeType:  "Root",
-		mountPath: "/",
+	g.nodes["root"] = &Node{
+		id:         "root",
+		nodeType:   "Root",
+		mountPath:  "/",
+		deviceType: "EmptyDir",
 	}
 
 	// Create dependencies between nodes
-	for _, node := range graph.nodes {
+	for _, node := range g.nodes {
 		if node.nodeType != "Entry" {
 			continue
 		}
 
-		// TODO: this is only if the deviceUri is chart-local
-		var parent *Node = graph.nodes["root"]
-		for _, iter := range graph.nodes {
-			if len(iter.mountPath) > len(parent.mountPath) {
-				if strings.HasPrefix(node.deviceUri, iter.mountPath) {
-					parent = iter
+		var parent *Node = g.nodes["root"]
+		if strings.Contains(node.deviceUri, "://") {
+			u, err := url.Parse(node.deviceUri)
+			if err != nil {
+				log.Println("deviceUri parsing failed.", err)
+				continue
+			}
+			parent = g.getImportNode(fmt.Sprintf("%s://%s", u.Scheme, u.Host))
+
+		} else {
+			for _, iter := range g.nodes {
+				if len(iter.mountPath) > len(parent.mountPath) {
+					if strings.HasPrefix(node.deviceUri, iter.mountPath) {
+						parent = iter
+					}
 				}
 			}
 		}
 
 		//log.Println("Node", node.mountPath, "depends on", parent.mountPath)
 		edgeId := extras.GenerateId()
-		graph.edges[edgeId] = &Edge{
+		g.edges[edgeId] = &Edge{
 			id:           edgeId,
 			upstreamId:   parent.id,
 			downstreamId: node.id,
