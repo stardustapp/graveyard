af45107f767bc40f1467ac0c9d104e5527548c9a
Daniel Lamando <dan+stardust@danopia.net>
2017-08-05 17:47:01 +0000

    skychart: New Device selector

--- app-suite/skychart/app.js
+++ app-suite/skychart/app.js
@@ -10,25 +10,48 @@ camel = (name) => name
   .replace(/-([a-z])/g,
            (x) => x[1].toUpperCase())
 
+var skylinks = {};
 enumeratePath = (chart, baseUri, maxDepth) => {
   if (maxDepth === undefined) maxDepth = 1;
   console.log('listing under', baseUri, 'depth', maxDepth);
   var basePath = '';
 
-  var linkMatch = baseUri.match(/^skylink:\/\/([a-z0-9\-]+)\.chart\.local(\/.*)?$/);
+  var linkMatch = baseUri.match(/^skylink(?:\+(wss?|https?))?:\/\/([a-z0-9\-:.]+)(\/.*)?$/);
   if (linkMatch) {
-    console.log('opening skychart', linkMatch[1], linkMatch[2]||'/');
-    const chartName = linkMatch[1];
 
-    const input = Skylink.String('chart-name', chartName);
-    const dest1 = '/tmp/autocomplete-local-chartapi-' + chartName;
-    const dest = '/tmp/autocomplete-local-chart-' + chartName;
-    return skychart.skylink.invoke('/pub/open/invoke', input, dest1)
-      .then(() => skychart.skylink.invoke(dest1 + '/browse/invoke', null, dest))
-      .then(() => skychart.skylink.enumerate(dest + linkMatch[2], {
+    var subProto = linkMatch[1];
+    if (subProto) {
+      // TODO: dial and fetch
+      var origin = subProto + '://' + linkMatch[2] + '/~~export';
+      if (subProto.startsWith('ws')) {
+        origin += '/ws';
+      }
+
+      console.log('Fetching', origin, 'skylink path', linkMatch[3]||'/');
+      skylinks[origin] = skylinks[origin] || new Skylink('', origin);
+      return skylinks[origin].enumerate(linkMatch[3]||'/', {
         maxDepth: maxDepth,
         includeRoot: false,
-      }));
+      })
+    }
+
+    var chartMatch = linkMatch[2].match(/^([a-z0-9\-]+)\.chart\.local$/);
+    if (chartMatch) {
+
+      console.log('opening skychart', chartMatch[1], linkMatch[3]||'/');
+      const chartName = chartMatch[1];
+
+      const input = Skylink.String('chart-name', chartName);
+      const dest1 = '/tmp/autocomplete-local-chartapi-' + chartName;
+      const dest = '/tmp/autocomplete-local-chart-' + chartName;
+      return skychart.skylink.invoke('/pub/open/invoke', input, dest1)
+        .then(() => skychart.skylink.invoke(dest1 + '/browse/invoke', null, dest))
+        .then(() => skychart.skylink.enumerate(dest + linkMatch[3], {
+          maxDepth: maxDepth,
+          includeRoot: false,
+        }));
+
+    }
 
   } else if (baseUri[0] === '/') {
     const upstream = chart.findClosestMount(baseUri);
@@ -36,14 +59,13 @@ enumeratePath = (chart, baseUri, maxDepth) => {
       const newPath = upstream.deviceUri + baseUri.slice(upstream.mountPath.length);
       return enumeratePath(chart, newPath, maxDepth);
     } else if (upstream.deviceType.endsWith('Mount')) {
-      const newPath = upstream.deviceUri + '/output-shape/props';
+      const newPath = upstream.deviceUri + '/output-shape/props' + baseUri.slice(upstream.mountPath.length);
       return enumeratePath(chart, newPath, maxDepth);
     }
   }
 
   return Promise.resolve([]);
 }
-
 
 Vue.component('edit-entry', {
   template: '#edit-entry',
@@ -58,108 +80,267 @@ Vue.component('edit-entry', {
       deviceType: this.entry.deviceType,
       deviceUri: this.entry.deviceUri,
 
-      deviceOpts: [],
-      devicePrefix: '',
+      parts: [],
+      deviceUri: '',
+
+      deviceSource: '',
+      externalOrigin: '',
+      inputShape: null,
+      inputData: null,
+
       mountOpts: [],
       mountOptVals: {},
-      deviceSuffix: '',
     };
   },
-  created() {
-    this.devicePrefix = this.chart.findClosestMount(this.deviceUri).mountPath;
-    this.deviceSuffix = this.deviceUri.slice(this.devicePrefix.length);
 
-    this.reloadOpts();
+  created() {
+    var selectedPath = '';
+
+    var uriMatch = this.entry.deviceUri.match(/^skylink(?:\+(wss?|https?))?:\/\/([a-z0-9\-:.]+)(\/.*)?$/);
+    if (uriMatch) {
+      this.deviceSource = 'External';
+      this.externalOrigin = this.entry.deviceUri.split('/', 3).join('/');
+      selectedPath = (uriMatch[3] || '');
+
+    } else if (this.entry.deviceUri.startsWith('/') || this.entry.deviceUri.length === 0) {
+      this.deviceSource = 'Internal';
+      selectedPath = this.entry.deviceUri;
+
+    } else {
+      throw new Error(`Device URI ${this.entry.deviceUri} can't be mapped`);
+    }
+
+    this.loadSource();
+    if (selectedPath.length) {
+      selectedPath.slice(1).split('/').forEach(name => {
+        this.selectNextName(name)
+      });
+    }
+
+    console.log('Restored device path', selectedPath);
+    this.deviceUri = selectedPath;
   },
+
   methods: {
 
-    reloadOpts() {
-      this.deviceOpts = [];
-      enumeratePath(this.chart, this.devicePrefix)
-        .then(x => x.map(y => {
-          y.Path = '/' + y.Name;
-          if (y.Type === 'Folder' && this.devicePrefix === '/drivers') {
-            y.Path += '/open'; // TODO!
-          }
-          return y;
-        }))
-        .then(x => this.deviceOpts = x)
-        .then(() => this.reloadMntOpts());
+    // reset path with new origin
+    loadSource() {
+      this.parts.length = 0;
+      var part = {
+        parent: '',
+        selected: '',
+        loaded: '',
+        choices: [],
+      };
+
+      if (this.deviceSource == 'External') {
+        part.baseUri = this.externalOrigin;
+        // TODO: inside BindLink, just hit the real thing
+        // TODO: (if it's remote, else transform ent)
+        enumeratePath(this.chart, this.externalOrigin, 1).then(list => {
+          list.forEach(entry => {
+            part.choices.push({
+              name: entry.Name,
+              type: entry.Type,
+            });
+          });
+        }, err => {
+          alert(`Error enumerating ${this.externalOrigin}:\n\n${err.message}`);
+        });
+
+      } else {
+        // internal to the chart
+
+        // locate a mount
+        const choices = new Set();
+        this.chart.entries.forEach(ent => {
+          if (ent.mountPath === this.mountPath) return;
+          var entParts = ent.mountPath.slice(1).split('/');
+          var nextPart = entParts[this.parts.length];
+          choices.add(nextPart);
+        });
+        choices.forEach(choice => {
+          part.choices.push({name: choice});
+        });
+
+      }
+      this.parts.push(part);
     },
 
-    reloadMntOpts() {
-      this.mountOpts = [];
-      if (!this.deviceType.endsWith('Mount')) return;
-      var prefixEnt = this.chart.entries.find(x => x.mountPath === this.devicePrefix);
-      if (prefixEnt) {
-        var inbetween = '';
-        if (prefixEnt.deviceType.endsWith('Mount')) {
-          inbetween = '/output-shape/props';
+    selectNextName(name) {
+      var part = this.parts[this.parts.length - 1];
+      part.selected = name;
+
+      var newPart = {
+        parent: part.parent + '/' + name,
+        selected: '',
+        loaded: '',
+        freetext: part.freetext,
+        choices: [],
+      };
+
+      // figure out if we're remote
+      var mount = this.chart.entries.find(ent => {
+        return newPart.parent === ent.mountPath;
+      });
+      if (mount) {
+        console.log('Setting baseUri to', mount.deviceUri, 'from', newPart.baseUri, 'at', newPart.parent);
+        if (mount.deviceType.endsWith('Mount')) {
+          newPart.baseUri = mount.deviceUri + '/output-shape/props';
+        } else {
+          newPart.baseUri = mount.deviceUri;
         }
+      } else if (part.baseUri) {
+        newPart.baseUri = part.baseUri + '/' + name;
+      }
 
-        const deviceUri = prefixEnt.deviceUri + inbetween + this.deviceSuffix;
-        enumeratePath(this.chart, deviceUri + '/input-shape', 4)
-          .then(list => {
-            var inputType = '';
-            const propMap = new Map();
-            list.forEach(entry => {
-              const parts = entry.Name.split('/');
-              if (entry.Name == 'type') {
-                inputType = entry.StringValue;
-              } else if (parts[0] === 'props' && parts.length === 2) {
-                propMap.set(parts[1], {
-                  name: parts[1],
-                  path: '/' + parts[1],
-                });
-                if (entry.Type === 'String') {
-                  // prop shorthand
-                  propMap.get(parts[1]).type = entry.StringValue;
-                }
-              } else if (parts[0] === 'props' && parts[2] === 'type' && parts.length === 3) {
-                propMap.get(parts[1]).type = entry.StringValue;
-              } else if (parts[0] === 'props' && parts[2] === 'optional' && parts.length === 3) {
-                propMap.get(parts[1]).optional = entry.StringValue === 'yes';
-              }
-            });
+      if ((newPart.baseUri || '').includes('/output-shape')) {
+        newPart.freetext = true;
 
-            if (inputType === 'String') {
-              if (this.entryName) {
-                skychart.skylink.loadString(this.chart.path + '/entries/' + this.entryName + '/device-input')
-                .then(x => this.mountOptVals = {__: x});
-              }
+      } else if (newPart.baseUri) {
+        // TODO: if the bindlink is is local, just transform ent
+        enumeratePath(this.chart, newPart.baseUri, 1).then(list => {
+          var hasInvoke = list.find(x => x.Name === 'invoke' && x.Type === 'Function');
+          if (hasInvoke) {
+            console.log('Located device at', newPart.parent, 'under', newPart.baseUri, 'with', hasInvoke);
+            this.loadDeviceInput(newPart.parent);
 
-              return [{
-                name: 'Input',
-                path: '__',
-                type: 'String',
-              }];
-            } else if (inputType === 'Folder') {
-              if (this.entryName) {
-                skychart.skylink.enumerate(this.chart.path + '/entries/' + this.entryName + '/device-input')
-                .then(x => {
-                  var vals = {};
-                  x.forEach(y => {
-                    if (y.Name) {
-                      vals['/'+y.Name] = y.StringValue || '';
-                    }
-                  });
-                  this.mountOptVals = vals;
-                });
-              }
-
-              var list = [];
-              propMap.forEach(prop => list.push(prop));
-              return list;
+            var idx = this.parts.indexOf(newPart);
+            if (idx >= 0) {
+              this.parts.splice(idx, 100);
             }
-            return [];
-          })
-          .then(x => this.mountOpts = x)
+          } else {
+            console.log('enumerated', list.length, 'items at', newPart.baseUri);
+            var found = false;
+            list.forEach(entry => {
+              if (entry.Name === newPart.selected) {
+                found = true;
+              }
+              newPart.choices.push({
+                name: entry.Name,
+                type: entry.Type,
+              });
+            });
+            if (newPart.selected && !found) {
+              var idx = this.parts.indexOf(newPart);
+              while (idx < this.parts.length) {
+                this.parts[idx].freetext = true;
+                idx++;
+              }
+            }
+          }
+        });
+
+      } else {
+        // locate a mount
+        const choices = new Set();
+        var mount;
+        this.chart.entries.forEach(ent => {
+          if (ent.mountPath === this.mountPath) return;
+          if (ent.mountPath.length >= newPart.parent.length && ent.mountPath.startsWith(newPart.parent)) {
+            var entParts = ent.mountPath.slice(1).split('/');
+            var nextPart = entParts[this.parts.length];
+            choices.add(nextPart);
+          }
+        });
+        choices.forEach(choice => {
+          newPart.choices.push({name: choice});
+        });
+      }
+
+      part.loaded = name;
+      console.log('Adding part', newPart);
+      this.parts.push(newPart);
+    },
+
+    partChanged(part) {
+      if (part.loaded === part.selected) return;
+      console.log('part changed', JSON.stringify(part));
+
+      var idx = this.parts.indexOf(part);
+      if (idx >= 0) {
+        this.parts.splice(idx+1, 100);
+      }
+      if (part.selected) {
+        this.selectNextName(part.selected);
+      }
+
+      for (var i = this.parts.length - 1; i > 0; i--) {
+        var part = this.parts[i];
+        if (part.selected) {
+          this.deviceUri = part.parent + '/' + part.selected;
+          console.log('Set deviceUri to', this.deviceUri);
+          return;
+        }
       }
     },
 
+    loadDeviceInput(uri) {
+      this.mountOpts = [];
+      enumeratePath(this.chart, uri + '/input-shape', 4).then(list => {
+        var inputType = '';
+        const propMap = new Map();
+        list.forEach(entry => {
+          const parts = entry.Name.split('/');
+          if (entry.Name == 'type') {
+            inputType = entry.StringValue;
+          } else if (parts[0] === 'props' && parts.length === 2) {
+            propMap.set(parts[1], {
+              name: parts[1],
+              path: '/' + parts[1],
+            });
+            if (entry.Type === 'String') {
+              // prop shorthand
+              propMap.get(parts[1]).type = entry.StringValue;
+            }
+          } else if (parts[0] === 'props' && parts[2] === 'type' && parts.length === 3) {
+            propMap.get(parts[1]).type = entry.StringValue;
+          } else if (parts[0] === 'props' && parts[2] === 'optional' && parts.length === 3) {
+            propMap.get(parts[1]).optional = entry.StringValue === 'yes';
+          }
+        });
+
+        if (inputType === 'String') {
+          if (this.entryName) {
+            skychart.skylink.loadString(this.chart.path + '/entries/' + this.entryName + '/device-input')
+              .then(x => this.mountOptVals = {__: x});
+          }
+
+          return [{
+            name: 'Input',
+            path: '__',
+            type: 'String',
+          }];
+        } else if (inputType === 'Folder') {
+          if (this.entryName) {
+            skychart.skylink.enumerate(this.chart.path + '/entries/' + this.entryName + '/device-input')
+              .then(x => {
+              var vals = {};
+              x.forEach(y => {
+                if (y.Name) {
+                  vals['/'+y.Name] = y.StringValue || '';
+                }
+              });
+              this.mountOptVals = vals;
+            });
+          }
+
+          var list = [];
+          propMap.forEach(prop => list.push(prop));
+          return list;
+        }
+        return [];
+      }).then(x => this.mountOpts = x)
+    },
+
+
     save() {
+      var deviceUri = this.deviceUri;
+      if (this.deviceSource === 'External') {
+        deviceUri = this.externalOrigin + deviceUri;
+      }
+
       const name = this.entryName || parseInt(Math.random().toString().slice(2)).toString(36);
-      const deviceUri = this.devicePrefix + this.deviceSuffix;
 
       var deviceInput;
       if (this.mountOpts.length) {
