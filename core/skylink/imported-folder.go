076eef047fbc5f5a6a274a2f825ffe201652502b
Daniel Lamando <apt@danopia.net>
2017-10-01 21:25:45 -0700

    skylink: Impl passthru subs on imported folders

--- core/skylink/imported-folder.go
+++ core/skylink/imported-folder.go
@@ -1,6 +1,7 @@
 package skylink
 
 import (
+	"errors"
 	"log"
 
 	"github.com/stardustapp/core/base"
@@ -89,7 +90,6 @@ func (e *importedFolder) Enumerate(enum *Enumerator, depth int) []nsEntry {
 	resp, err := e.svc.transport.exec(req)
 	if err != nil {
 		log.Println("nsimport folder enumerate err:", err)
-		return nil
 	}
 
 	if !resp.Ok || resp.Output == nil {
@@ -97,4 +97,68 @@ func (e *importedFolder) Enumerate(enum *Enumerator, depth int) []nsEntry {
 		return nil
 	}
 	return resp.Output.Children
+}
+
+var _ Subscribable = (*importedFolder)(nil)
+
+func (e *importedFolder) Subscribe(s *Subscription) (err error) {
+	req := nsRequest{
+		Op:    "subscribe",
+		Path:  e.prefix,
+		Depth: s.maxDepth,
+	}
+
+	resp, err := e.svc.transport.exec(req)
+	if err != nil {
+		return errors.New("nsimport folder subscribe err:")
+	}
+
+	if resp.Channel != nil {
+		go func(inC <-chan nsResponse, outC chan<- Notification) {
+			log.Println("imported-folder: Starting subscription pump from", e.prefix)
+			for pkt := range inC {
+				if pkt.Output == nil {
+					log.Println("imported-folder WARN: sub got a packet without an output.", pkt)
+					break
+				}
+				if pkt.Output.Name != "notif" {
+					log.Println("imported-folder WARN: sub got a non-notif packet:", pkt.Output.Name)
+					break
+				}
+
+				switch pkt.Status {
+				case "Next":
+					var notif Notification
+					for _, field := range pkt.Output.Children {
+						switch field.Name {
+						case "type":
+							notif.Type = field.StringValue
+						case "path":
+							notif.Path = field.StringValue
+						case "entry":
+							notif.Entry = convertEntryFromApi(&field)
+						default:
+							log.Println("imported-folder WARN: sub got weird Next field,", field.Name)
+						}
+					}
+					log.Println("imported-folder: sub notification:", notif)
+					outC <- notif
+
+					// any non-Next status is terminal
+				default:
+					log.Println("imported-folder: sub got unknown packet", pkt.Status)
+				}
+			}
+			log.Println("imported-folder: Completed subscription pump from", e.prefix)
+			close(outC)
+		}(resp.Channel, s.streamC)
+	}
+
+	if resp.Status == "Ok" {
+		return nil
+	} else if resp.Output != nil && resp.Output.Type == "String" {
+		return errors.New("Subscription failed. Cause: " + resp.Output.StringValue)
+	} else {
+		return errors.New("Subscription attempt wasn't Ok, was " + resp.Status)
+	}
 }
