62b74dc646b857f02bbe8d9800ac0c133875fc04
Daniel Lamando <dan+pixelbook@danopia.net>
2018-02-24 13:16:47 -0800

    Add websocket serving to nsexport

--- nodejs-runtime/src/nsexport.js
+++ nodejs-runtime/src/nsexport.js
@@ -1,43 +1,10 @@
 const restify = require('restify');
+const {Watershed} = require('watershed');
 
 exports.NsExport = class NsExport {
   constructor(namespace) {
     this.namespace = namespace;
-
-    this.server = restify.createServer({
-      name: 'stardust-nodejs-runtime',
-    });
-    this.server.use(restify.plugins.bodyParser());
-
-    this.server.post('/~~export', (req, res, next) => {
-      const send = (ok, output) => {
-        res.send(200, {
-          Ok: ok,
-          Output: output,
-        });
-        return next();
-      };
-
-      try {
-        const output = this.processOp(req.body);
-        send(true, output);
-
-      } catch (err) {
-        console.log('==! Operation failed with', err.message);
-        send(false, {
-          Type: 'String',
-          Name: 'error-message',
-          StringValue: err.message,
-        });
-      }
-    });
-  }
-
-  listen(port=9234) {
-    this.port = port;
-    this.server.listen(port, () => {
-      console.log('%s listening at %s', this.server.name, this.server.url);
-    });
+    this.configureServer();
   }
 
   processOp(request) {
@@ -61,5 +28,82 @@ exports.NsExport = class NsExport {
       default:
         throw new Error(`Server doesn't implement ${Op} operation`);
     }
+  }
+
+  configureServer() {
+    this.server = restify.createServer({
+      name: 'stardust-nodejs-runtime',
+      handleUpgrades: true,
+    });
+    this.server.use(restify.plugins.bodyParser());
+
+    this.server.post('/~~export', (req, res, next) => {
+      const send = (ok, output) => {
+        console.log('<-- op was', ok ? 'okay' : 'not ok');
+        res.send(200, {
+          Ok: ok,
+          Output: output,
+        });
+        return next();
+      };
+
+      try {
+        const output = this.processOp(req.body);
+        send(true, output);
+
+      } catch (err) {
+        console.log('!!! Operation failed with', err.message);
+        send(false, {
+          Type: 'String',
+          Name: 'error-message',
+          StringValue: err.message,
+        });
+      }
+    });
+
+    var ws = new Watershed();
+    this.server.get('/~~export/ws', (req, res, next) => {
+      console.log('upgrading socket')
+      if (!res.claimUpgrade) {
+        next(new Error('Connection Must Upgrade For WebSockets'));
+        return;
+      }
+
+      var upgrade = res.claimUpgrade();
+      var shed = ws.accept(req, upgrade.socket, upgrade.head);
+
+      const send = (ok, output) => {
+        console.log('<-- op was', ok ? 'okay' : 'not ok');
+        shed.send(JSON.stringify({
+          Ok: ok,
+          Output: output,
+        }));
+      };
+
+      shed.on('text', (msg) => {
+        const request = JSON.parse(msg);
+        try {
+          const output = this.processOp(request);
+          send(true, output);
+
+        } catch (err) {
+          const stackSnip = err.stack.split('\n').slice(0,4).join('\n');
+          console.log('!!! Operation failed with', stackSnip);
+          send(false, {
+            Type: 'String',
+            Name: 'error-message',
+            StringValue: err.message,
+          });
+        }
+      });
+
+      next(false);
+    });
+  }
+
+  listen(port=9234) {
+    this.port = port;
+    this.server.listen.sync(this.server, port);
+    console.log('%s listening at %s', this.server.name, this.server.url);
   }
 }
