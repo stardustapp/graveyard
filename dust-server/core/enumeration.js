d9161f91c724f33a9a05ea4c1812c8b4ba5c783d
Daniel Lamando <apt@danopia.net>
2018-05-16 00:37:50 -0700

    core: allow EnumWriter chaining, catch misuse bugs

--- dust-server/core/enumeration.js
+++ dust-server/core/enumeration.js
@@ -8,6 +8,7 @@ class EnumerationWriter {
   visit(literal) {
     literal.Name = this.names.map(encodeURIComponent).join('/');
     this.entries.push(literal);
+    return this;
   }
 
   canDescend() {
@@ -16,12 +17,18 @@ class EnumerationWriter {
 
   descend(name) {
     this.names.push(name);
+    return this;
   }
   ascend() {
+    if (this.names.length === 0)
+      throw new Error(`BUG: EnumerationWriter ascended above its root`);
     this.names.pop();
+    return this;
   }
 
   toOutput() {
+    if (this.names.length > 0)
+      throw new Error(`BUG: EnumerationWriter asked to serialize, but is still descended`);
     return new FolderLiteral('enumeration', this.entries);
   }
 }
