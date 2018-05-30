61758e8333a21b0155a02377718c4d85595c339f
Daniel Lamando <dan+pixelbook@danopia.net>
2018-05-29 23:25:21 -0700

    Serve actual files over HTTP (text only, lol)

--- dust-server/core/http-server.js
+++ dust-server/core/http-server.js
@@ -43,8 +43,32 @@ class VirtualHost {
   static /*async*/ fromManifest() {
     return new Promise(r => 
       chrome.runtime.getPackageDirectoryEntry(r))
-      .then(x => new WebFilesystemMount({root: x}))
+      .then(x => new WebFilesystemMount({
+        entry: x,
+        prefix: 'platform-apps/',
+      }))
       .then(x => new VirtualHost('localhost', x));
+  }
+
+  async handleGET(req, respond) {
+    const target = await this.folderDevice.getEntry(req.uri || '/');
+    if (!target) {
+      respond({error: 'not-found'}, 404);
+    } else if (target.Type == 'Blob') {
+      const decoded = atob(target.Data);
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(decoded.length);
+      var ia = new Uint8Array(ab);
+      for (var i = 0; i < decoded.length; i++) {
+          ia[i] = decoded.charCodeAt(i);
+      }
+
+      respond(decoded, 200, target.Mime);
+
+    } else {
+      respond(target);
+    }
   }
 }
 
@@ -59,46 +83,48 @@ class HttpWildcardHandler extends WSC.BaseHandler {
     this.httpServer = httpServer;
   }
 
-  sendResponse(data, status=200) {
-    const payload = JSON.stringify(data, null, 2);
+  sendResponse(data, status=200, type=null) {
+    const payload = data.constructor == Object ? JSON.stringify(data, null, 2) : data;
 
     this.responseLength = payload.length;
     this.setHeader('Date', moment.utc().format('ddd, DD MMM YYYY HH:mm:ss [GMT]'));
     this.setHeader('Server', HttpServer.SERVER_HEADER);
-    this.setHeader('Content-Type', 'application/json');
+    this.setHeader('Content-Type', type || 'application/json');
     this.writeHeaders(status);
     this.write(payload);
     this.finish();
   }
 
   async get() {
+    const {headers, uri, method} = this.request;
+    const {localAddress, localPort, peerAddress, peerPort} =
+        await new Promise(r => chrome.sockets.tcp.getInfo(
+            this.request.connection.stream.sockId, r));
+
+    const meta = {method, uri, headers,
+      ip: {localAddress, localPort, peerAddress, peerPort}};
+
     try {
-      const {headers, path, method} = this.request;
-      const {localAddress, localPort, peerAddress, peerPort} =
-          await new Promise(r => chrome.sockets.tcp.getInfo(
-              this.request.connection.stream.sockId, r));
-
-      const meta = {method, path, headers,
-        ip: {localAddress, localPort, peerAddress, peerPort}};
-
       if (!headers.host) {
+        console.log(`GET //${headers.host}${uri}`, 400);
         return this.sendResponse({
           success: false,
           error: 'bad-request',
           message: 'Your browser sent a request that this server could not understand.',
           cause: 'The "Host" HTTP header is required.',
-        });
+        }, 400);
       }
 
       const hostMatch = headers.host.match(
   /^(?:([0-9]{1,3}(?:\.[0-9]{1,3}){3})|\[([0-9a-f:]+(?:[0-9]{1,3}(?:\.[0-9]{1,3}){3})?)\]|((?:[a-z0-9_.-]+\.)?[a-z]+))(?::(\d+))?$/i);
       if (!hostMatch) {
+        console.log(`GET //${headers.host}${uri}`, 400);
         return this.sendResponse({
           success: false,
           error: 'bad-request',
           message: 'Your browser sent a request that this server could not understand.',
           cause: 'The "Host" HTTP header could not be parsed. If your request is reasonable, please file a bug.',
-        });
+        }, 400);
       }
       const [_, ipv4, ipv6, hostname, port] = hostMatch;
 
@@ -112,20 +138,22 @@ class HttpWildcardHandler extends WSC.BaseHandler {
         if (vhost) {
           return await vhost.handleGET(meta, this.sendResponse.bind(this));
         } else {
+          console.log(`GET //${headers.host}${uri}`, 506);
           return this.sendResponse({
             success: false,
             error: 'domain-not-found',
             message: `The website you tried to access doesn't exist here`,
             cause: `This server doesn't have a website configured for the hostname ${hostname}. If this is your domain, go ahead and claim `,
-          });
+          }, 506);
         }
       }
     } catch (err) {
+      console.log(`GET //${headers.host}${uri}`, 500, err);
       return this.sendResponse({
         success: false,
         error: 'internal-error',
-        message: `The server failed to response`,
-        cause: `File a bug! :D ${err.name} ${err.message}`,
+        message: `The server failed to respond`,
+        cause: `${err.name}: ${err.message}`,
       }, 500);
       throw err;
     }
--- dust-server/core/mounts/web-filesystem.js
+++ dust-server/core/mounts/web-filesystem.js
@@ -1,61 +1,74 @@
-const DirectoryEntryApi = new PlatformApi('web directory entry');
-const FileEntryApi = new PlatformApi('web file entry');
-[DirectoryEntryApi, FileEntryApi].forEach(api => api
-  .getter('/is file', Boolean, () => this.inner.isFile)
-  .getter('/is directory', Boolean, () => this.inner.isDirectory)
-  .getter('/name', String, () => this.inner.name)
-  .getter('/full path', String, () => this.inner.fullPath)
-);
-
-DirectoryEntryApi.function('/get directory', {
-  input: {
-    path: String,
-    create: false,
-    exclusive: false,
-  },
-  output: DirectoryEntryApi,
-  impl({path, create, exclusive}) {
-    return new Promise((resolve, reject) => {
-      this.entry.getDirectory(path,
-          {create, exclusive},
-          ent => resolve(new DirectoryEntryApi(ent)),
-          err => reject(err));
-    });
-  }
-});
-DirectoryEntryApi.function('/get file', {
-  input: {
-    path: String,
-    create: false,
-    exclusive: false,
-  },
-  output: FileEntryApi,
-  impl({path, create, exclusive}) {
-    return new Promise((resolve, reject) => {
-      this.entry.getFile(path,
-          {create, exclusive},
-          ent => resolve(new FileEntryApi(ent)),
-          err => reject(err));
-    });
-  }
-});
-
-DirectoryEntryApi.compile();
-FileEntryApi.compile();
-
 class WebFilesystemMount {
   constructor(opts) {
     console.log('web filesystem inited with', opts);
 
-    this.root = opts.root;
-    if (!this.root.getDirectory) {
-      throw new Error(`WebFilesystemMount given root without getDirectory()`);
+    this.entry = opts.entry;
+    this.prefix = opts.prefix || '';
+    if (!this.entry.getDirectory) {
+      throw new Error(`WebFilesystemMount given entry without getDirectory()`);
     }
 
-    this.api = DirectoryEntryApi.construct(this.root);
+    //this.api = DirectoryEntryApi.construct(this.root);
   }
 
-  /*async*/ getEntry(path) {
-    return this.api.getEntry(path);
+  async getEntry(path) {
+    const subPath = path.slice(1);
+    // Use trailing slash to signal for a directory instead
+    // TODO: how annoying is that?
+    if (path.endsWith('/')) {
+      return await new Promise((resolve, reject) =>
+          this.entry.getDirectory(this.prefix+subPath, {create: false}, resolve, reject))
+        .then(d => {
+          const reader = d.createReader();
+          let entries = [];
+          return new Promise((resolve, reject) => {
+            const getEntries = function() {
+              reader.readEntries(results => {
+                if (results.length) {
+                  entries = entries.concat(results);
+                  getEntries();
+                } else {
+                  resolve({d, entries});
+                }
+              }, reject);
+            };
+            getEntries();
+          });
+        }).then(({d, entries}) => {
+          return new FolderLiteral(d.name, entries.map(e => {
+            if (e.isDirectory) return new FolderLiteral(e.name);
+            if (e.isFile) return new StringLiteral(e.name);
+          }));
+        }, err => {
+          if (err.name === 'NotFoundError')
+            return null;
+          return err;
+        });
+    } else {
+      return await new Promise((resolve, reject) =>
+          this.entry.getFile(this.prefix+subPath, {create: false}, resolve, reject))
+        .then(f => {
+          return new Promise((resolve, reject) =>
+            f.file(file => resolve({f, file}), reject));
+        })
+        .then(({f, file}) => {
+          var reader = new FileReader();
+          return new Promise((resolve, reject) => {
+            reader.onloadend = function(e) {
+              if (this.error) {
+                reject(this.error);
+              } else {
+                const dataIdx = this.result.indexOf(',')+1;
+                resolve(new BlobLiteral(f.name, this.result.slice(dataIdx), file.type));
+              }
+            };
+            reader.readAsDataURL(file);
+          });
+        }, err => {
+          if (err.name === 'NotFoundError')
+            return null;
+          return err;
+        });
+    }
   }
 }
