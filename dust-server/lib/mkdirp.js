956e2b81ba350b4a5b0598921a1e340ced283128
Daniel Lamando <apt@danopia.net>
2018-07-28 23:57:24 -0700

    Update mkdirp impl for new get failure semantics

--- dust-server/lib/mkdirp.js
+++ dust-server/lib/mkdirp.js
@@ -1,7 +1,12 @@
 async function mkdirp(device, path, justEnsure=false) {
-  const entry = await device.getEntry(path);
-  const literal = entry && await entry.get();
-  if (literal) return false;
+  try {
+    entry = await device.getEntry(path);
+    const literal = await entry.get();
+    if (literal) return false; // exists!
+  } catch (err) {
+    if (!(err.message.includes(`doesn't exist, can't be gotten`)))
+      throw err;
+  }
 
   if (justEnsure)
     throw new Error(`checked for folder at "${path}" but that path wasn't found`);
@@ -12,15 +17,21 @@ async function mkdirp(device, path, justEnsure=false) {
     curParts.push(part);
     const curPath = '/'+curParts.join('/');
 
-    const curEntry = await device.getEntry(curPath);
-    const literal = curEntry && await curEntry.get();
-    if (literal) continue; // exists!
+    let entry;
+    try {
+      entry = await device.getEntry(curPath);
+      const literal = await entry.get();
+      if (literal) continue; // exists!
+    } catch (err) {
+      if (!(err.message.includes(`doesn't exist, can't be gotten`)))
+        throw err;
+    }
 
-    if (!curEntry || !curEntry.put)
+    if (!entry || !entry.put)
       throw new Error('Failed to auto-create folder', curPath, `because it wasn't writable`);
 
     console.log('mkdirp creating folder', curPath);
-    const ok = await curEntry.put(new FolderLiteral(decodeURIComponent(part)));
+    const ok = await entry.put(new FolderLiteral(decodeURIComponent(part)));
     if (!ok)
       throw new Error('Failed to auto-create folder', curPath, `- just didn't work`);
   }
