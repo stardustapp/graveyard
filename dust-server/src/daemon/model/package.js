9fc33f3fd58d7c98d262f245f3ffa9e8218cfefc
Daniel Lamando <dan+pixelbook@danopia.net>
2018-08-12 10:04:01 -0700

    Move literally every file except README

---
+++ dust-server/src/daemon/model/package.js
@@ -0,0 +1,105 @@
+class Package {
+  constructor(record) {
+    this.record = record;
+    this.webroot = null;
+    this.ready = this.load();
+  }
+
+  async load() {
+    if (this.record.sourceUri.startsWith('platform://')) {
+      const parts = this.record.sourceUri.split('://');
+      const pkgRoot = await new Promise(r =>
+        chrome.runtime.getPackageDirectoryEntry(r));
+      this.webroot = new WebFilesystemMount({
+        entry: pkgRoot,
+        prefix: `packages/${parts[1]}/webapp/`,
+      });
+    } else {
+      throw new Error(`Package used unrecognized source ${this.record.sourceUri}`);
+    }
+  }
+
+  wantsAlwaysOn() {
+    return Object
+      .keys(this.record.workloads)
+      .map(x => this.record.workloads[x])
+      .some(w => w.type === 'daemon');
+  }
+
+  createAppInstall(account, appKey, opts) {
+    if (!appKey.match(/^[a-z][a-z0-9]+$/i)) {
+      throw new Error(`App keys must be alphanumeric`);
+    }
+    console.log('creating app', appKey, 'from package', this.record.displayName, 'for', account.address());
+
+    // Start with literally just the source code
+    const mounts = [
+      {
+        type: 'bind',
+        target: '/source',
+        source: this.record.sourceUri,
+      },
+    ];
+
+    let alwaysOn = false;
+    const workKeys = Object.keys(this.record.workloads);
+    if (workKeys.length) {
+      mounts.push({
+        type: 'bind',
+        target: '/workloads',
+        source: `/apps/${appKey}/workloads`,
+        skipIfMissing: true,
+      });
+
+      // Some workloads want to autostart on system boot
+      if (workKeys.some(w => this.record.workloads[w].type === 'daemon')) {
+        alwaysOn = true;
+      }
+    }
+
+    for (const mountPoint in this.record.mounts) {
+      const mountDef = this.record.mounts[mountPoint];
+      switch (mountDef.type) {
+        case 'scoped':
+          // Scoped is supposed to be private to the specific application, by appKey
+          // We just allow these by default
+          if (!['config', 'persist'].includes(mountDef.flavor)) {
+            throw new Error(`Package used unrecognized scoped-mount flavor "${mountDef.flavor}"`);
+          }
+          mounts.push({
+            type: 'bind',
+            target: mountPoint,
+            source: `/${mountDef.flavor}/${appKey}`,
+            createIfMissing: true, // don't fail if it doesn't exist already
+          });
+          break;
+        case 'bind':
+          // Bind is to receive arbitrary entries from the user's environment or the world, as input
+          // The user must explicitly allow the path because they are allowing data access
+          const fieldKey = `mount-${encodeURIComponent(mountPoint)}`;
+          if (fieldKey in opts) {
+            const source = opts[fieldKey];
+            if (!source) {
+              throw new Error(`Mountpoint ${mountPoint} given empty path`);
+            }
+            mounts.push({
+              type: 'bind',
+              target: mountPoint,
+              source,
+            });
+            break;
+          }
+          throw new Error(`Package mountpoint ${mountPoint} requested a source path, but none was given`);
+        default:
+          throw new Error(`Package requested unrecognized mount type "${mountDef.type}"`);
+      }
+    }
+
+    return {
+      appKey,
+      pid: this.record.pid,
+      mounts,
+      alwaysOn,
+    };
+  }
+}
---
+++ dust-server/src/runtimes/lua.js
@@ -0,0 +1,135 @@
+this.window = this;
+importScripts(
+  '/src/core/api-entries.js',
+  '/src/core/environment.js',
+  '/src/core/enumeration.js',
+  '/src/core/utils.js',
+
+  '/src/devices/tmp.js',
+  '/src/devices/network-import.js',
+
+  '/src/webapp/core/data/channel.js',
+  '/src/webapp/core/data/subs/_base.js',
+  '/src/webapp/core/data/subs/single.js',
+  '/src/webapp/core/skylink/client.js',
+  '/src/webapp/core/skylink/ns-convert.js',
+
+  '/src/kernel/caching.js',
+  '/src/kernel/tracing.js',
+  '/src/kernel/mkdirp.js',
+  '/src/kernel/path-fragment.js',
+
+  '/vendor/libraries/fengari.js',
+  '/vendor/libraries/moment.js',
+  //'/vendor/libraries/bugsnag.js',
+);
+importScripts(
+  '/src/core/nsexport.js',
+  '/src/core/platform-api.js',
+
+  '/src/kernel/runtime-slave-worker.js',
+  '/src/kernel/lua-machine.js',
+  '/src/kernel/lua-api.js',
+);
+delete this.window;
+
+const StateEnvs = new LoaderCache(id => {
+  const env = new Environment();
+  env.bind('', new TemporaryMount());
+  return env;
+});
+
+class Workload {
+  constructor({basePath, spec, wid}) {
+    this.basePath = basePath;
+    this.spec = spec;
+    this.wid = wid;
+
+    this.env = new Environment();
+    this.ready = this.init();
+  }
+
+  async init() {
+    await this.env.bind('/session', runtime.deviceForKernelPath(this.basePath));
+    await this.env.bind('/session/state', await StateEnvs.getOne('x', 'x'));
+    const sourceEntry = await this.env.getEntry('/session/source/'+this.spec.sourceUri);
+
+    this.machine = new LuaMachine(this.env.pathTo('/session'));
+    this.thread = this.machine.startThread();
+
+    if (sourceEntry.subscribe) {
+      const rawSub = await sourceEntry.subscribe();
+      await new Promise(resolve => {
+        const sub = new SingleSubscription(rawSub);
+        sub.forEach(literal => {
+          const source = atob(literal.Data);
+          this.thread.compile(source);
+
+          resolve && resolve();
+          resolve = null;
+        });
+      });
+
+    } else {
+      const literal = await sourceEntry.get();
+      const source = atob(literal.Data);
+      this.thread.compile(source);
+    }
+    return this;
+  }
+
+  start(input) {
+    const completion = this.thread.run(input)
+      .catch(err => {
+        // restart the lua thread since it's probably crashed
+        // TODO: cleanly stop old thread, or recover the lua thread
+        console.warn('Recreating Lua workload because of crash');
+        const oldThread = this.thread;
+        this.thread = this.machine.startThread();
+        this.thread.compile(oldThread.sourceText);
+        return Promise.reject(err);
+      });
+
+    return {
+      completion,
+    };
+  }
+
+  async stop(evt) {
+    console.warn('stopping workload', this);
+    return this;
+  }
+}
+
+const runtime = new RuntimeSlaveWorker(api => {
+  const workloads = new Map;
+
+  api.set('start daemon', async input => {
+    const workload = new Workload(input);
+    workloads.set(input.wid, workload);
+    await workload.ready;
+    workload.start();
+    return;
+  });
+  api.set('stop daemon', async input => {
+    const workload = workloads.get(input.wid);
+    if (!workload)
+      throw new Error('BUG: stop requested for unregistered daemon.', input);
+    workloads.delete(input.wid);
+    await workload.stop(input.reason);
+    return;
+  });
+
+  api.set('load function', async input => {
+    const workload = new Workload(input);
+    workloads.set(input.wid, workload);
+    return;
+  });
+  api.set('run function', async input => {
+    const workload = workloads.get(input.wid);
+    if (!workload)
+      throw new Error('BUG: run requested for unregistered function.', input);
+    const handle = workload.start(input.input);
+    return handle.completion;
+  });
+});
