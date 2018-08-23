6a408481ad6fab4047fa9e1fcca07548b3294977
Daniel Lamando <apt@danopia.net>
2018-08-23 01:15:21 -0700

    Handle errors parsing POST bodies, send client error

--- dust-server/src/core/wsc-skylink.js
+++ dust-server/src/core/wsc-skylink.js
@@ -17,23 +17,32 @@ class SkylinkPostHandler extends WSC.BaseHandler {
     this.skylink = skylink;
   }
 
-  sendResponse(data) {
+  sendResponse(data, status=200) {
     const payload = JSON.stringify(data);
 
     this.responseLength = payload.length;
     this.setHeader('Date', moment.utc().format('ddd, DD MMM YYYY HH:mm:ss [GMT]'));
     this.setHeader('Server', HttpServer.SERVER_HEADER);
     this.setHeader('Content-Type', 'application/json');
-    this.writeHeaders(200);
+    this.writeHeaders(status);
     this.write(payload);
     this.finish();
   }
 
   async post(path) {
-    // Parse up the submitted JSON
-    var request = JSON.parse(
-      String.fromCharCode.apply(null,
-        new Uint8Array(this.request.body)));
+    try {
+      // Parse up the submitted JSON
+      var request = JSON.parse(
+        String.fromCharCode.apply(null,
+          new Uint8Array(this.request.body)));
+    } catch (err) {
+      this.sendResponse({
+        error: true,
+        message: `Couldn't parse JSON from your POST body`,
+        detail: err.message,
+      }, 400);
+      throw err;
+    }
 
     const response = await this.skylink.processFrame(request);
     this.sendResponse(response);
