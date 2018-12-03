ae5934a59994e007c390b05ac500b14980e2334d
Daniel Lamando <apt@danopia.net>
2018-12-02 18:23:52 -0800

    revert software-db to working state

--- dust-server/src/apps-sw/software-db.js
+++ dust-server/src/apps-sw/software-db.js
@@ -4,116 +4,20 @@
 async function MigrateDatabase(upgradeDB) {
   switch (upgradeDB.oldVersion) {
     case 0:
-      const repos = upgradeDB.createObjectStore(
-        'repos', { keyPath: 'repoId' });
-      repos.createIndex('by origin',
-        'origin', { unique: false });
-
-      const objects = upgradeDB.createObjectStore(
-        'objects', { keyPath: ['repoId', 'objId', 'rev'] });
-      objects.createIndex('repo refs',
-        'refs', // [repoId, refType, refName]
-        { unique: true, multiEntry: true });
+      const packages = upgradeDB.createObjectStore('packages', { keyPath: 'pid' });
+      //packages.createIndex('deps', 'deps', { unique: false, multiEntry: true });
+      const resources = upgradeDB.createObjectStore('resources', { keyPath: 'rid' });
+      resources.createIndex('pid', 'pid', { unique: false });
+      resources.createIndex('pidPath', ['pid', 'path'], { unique: true });
+      const datums = upgradeDB.createObjectStore('datums', { keyPath: 'did' });
+      datums.createIndex('pid', 'pid', { unique: false });
   }
 }
 
 const HardCodedDatabaseVersion = 1;
 async function OpenSoftwareDatabase() {
   const db = await idb.open('software', HardCodedDatabaseVersion, MigrateDatabase);
-  return new RepositoryStore(db);
-}
-
-class RepositoryStore {
-  constructor(db) {
-    this.idb = db;
-    this.typeCache = new LoaderCache(this.loadType.bind(this));
-  }
-  async getAllRepositories() {
-    const data = await this.idb
-      .transaction('repos')
-      .objectStore('repos')
-      .getAll();
-    return data;
-  }
-  async getRepositoryRef(repoId, refType, refName) {
-    const data = await this.idb
-      .transaction('objects')
-      .objectStore('objects')
-      .index('repo refs')
-      .get([repoId, refType, refName]);
-    return data;
-  }
-  async initRepository(origin) {
-    const tx = this.idb.transaction(['repositories', 'objects'], 'readwrite');
-    const record = {
-      repoId: randomString(8),
-      createdAt: new Date,
-      origin: origin,
-    };
-    tx.objectStore('repositories')
-      .add(repoRecord);
-    await tx.complete;
-    return record;
-  }
-  async storeObjectRevision({repoId, objType, objId, version, data}) {
-    if (!repoId) throw new Error(`No repository ID given`);
-    if (!objType) throw new Error(`No object type given`);
-    const isNew = objId === null;
-
-    if (isNew) {
-      if (version !== null) throw new Error(
-        `version cannot be specified when creating an object`);
-      objId = randomString(10);
-      version = 0;
-    }
-
-    const tx = this.idb.transaction('objects', 'readwrite');
-    // get overlapping versions
-    const existingVersions = await tx
-      .objectStore('objects')
-      .getAllKeys(IDBKeyRange.bound(
-        [repoId, objId, version],
-        [repoId, objId, Number.MAX_VALUE]));
-    if (isNew && existingVersions.length) throw new Error(
-      `Random object ID chosen for new object had existing conflict in repo, try again`);
-    if (existingVersions.find(v => v[2] > version)) throw new Error(
-      `A newer version of this object already exists in repo, redo your change`);
-
-    const record = {
-      repoId, objId, version,
-      objType, data,
-      createdAt: new Date,
-      refs: [],
-    };
-    this.validateObject(record);
-
-    tx.objectStore('objects')
-      .add(record);
-
-    await tx.complete;
-    return record;
-  }
-
-  validateObject(record) {
-    switch (record.objType) {
-      case 'Tree':
-        console.log('validating tree:', record);
-      case 'Resource':
-        switch (record.resType) {
-          case 'Template':
-          case 'Application':
-        }
-      default:
-        console.error(record, type);
-        throw new Error(`Unknown Object ${record.objType}`);
-    }
-  }
-}
-
-class DbObject {
-  constructor(record) {
-    this.record = record;
-  }
+  return new SoftwareDatabase(db);
 }
 
 class SoftwareDatabase {
@@ -131,7 +35,7 @@ class SoftwareDatabase {
       .objectStore('packages')
       .get(packageId);
     if (!record) throw new Error(`Package not found: ${packageId}`);
-    console.log('loaded package', record);
+    console.log('loaded package', packageId, 'meta:', record);
     return new SoftwarePackage(this, record);
   }
 
@@ -143,22 +47,15 @@ class SoftwareDatabase {
   }
 
   async createPackage(meta) {
-    const record = {
-      version: 1,
-      pid: randomString(10),
-      createdAt: new Date,
-      metadata: meta,
-      resources: [],
-      rids: [],
-    };
+    meta.pid = randomString(10);
 
     const tx = this.idb
       .transaction('packages', 'readwrite');
     tx.objectStore('packages')
-      .add(record);
+      .add(meta);
 
     await tx.complete;
-    return record;
+    return meta;
   }
 }
 
@@ -173,16 +70,15 @@ function randomString(bytes=10) { // 32 for a secret
 }
 
 class SoftwarePackage {
-  constructor(sdb, record) {
+  constructor(sdb, meta) {
     this.sdb = sdb;
-    this.record = record;
-    //this.resourceCache = new LoaderCache(this.loadResource.bind(this));
+    this.meta = meta;
+    this.resourceCache = new LoaderCache(this.loadResource.bind(this));
   }
 
-  //getResource(path) {
-    //return this.resourceCache.getOne(path, path);
-  //}
-
+  getResource(path) {
+    return this.resourceCache.getOne(path, path);
+  }
   // name.type - within the package
   // package:name.type - from a dep
   // TODO: type guessing:
