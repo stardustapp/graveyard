aac71fb7426ef5fd3cadf44d90abfe372086c126
Daniel Lamando <apt@danopia.net>
2019-03-18 19:24:38 -0700

    Wire empty level store into nodejs lifecycle

--- dust-server/nodejs/shims/src.daemon.database.js
+++ dust-server/nodejs/shims/src.daemon.database.js
@@ -1,6 +1,62 @@
 const fs = require('fs');
 const os = require('os');
-const rimraf = require('rimraf');
+const {promisify} = require('util');
+
+const rimraf = promisify(require('rimraf'));
+
+const level = promisify(require('level'));
+const sub = require('subleveldown');
+const levelgraph = require('levelgraph');
+
+function EstablishTempDir() {
+  const tempDir = fs.realpathSync(os.tmpdir());
+  const dbPath = fs.mkdtempSync(tempDir+'/dust-data_');
+
+  process.once('beforeExit', async (code) => {
+    try {
+      await rimraf(dbPath, {
+        disableGlob: true,
+      });
+    } catch (err) {
+      console.error(`\r!-> Failed to clean server memory on shutdown: ${err.message}`);
+      console.error(`Temporary data may still exist in`, dbPath);
+    }
+    console.log(`Cleaned server memory from disk.`);
+  });
+
+  return dbPath;
+}
+
+function AsssertWritable(fsPath) {
+  try {
+    fs.accessSync(fsPath, fs.constants.W_OK);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log('\r--> Creating new data directory', fsPath);
+    } else {
+      throw new Error(`Data path ${fsPath} not writable: ${err.message}`);
+    }
+  }
+}
+
+class ServerDatabase {
+  constructor(db) {
+    this.rawStore = db;
+    this.graph = levelgraph(sub(db, 'graph'));
+  }
+
+  static async open(dataPath) {
+    AsssertWritable(dataPath);
+    const db = await level(dataPath);
+    return new ServerDatabase(db);
+  }
+
+  close() {
+    const p = this.rawStore.close();
+    this.rawStore = null;
+    return p;
+  }
+}
 
 let db = null;
 OpenSystemDatabase = async function (argv) {
@@ -9,40 +65,12 @@ OpenSystemDatabase = async function (argv) {
     return db;
   }
 
-  let dbPath = null;
-  if (argv.dataPath) {
-    dbPath = argv.dataPath;
-  } else {
-    const tempDir = fs.realpathSync(os.tmpdir());
-    dbPath = fs.mkdtempSync(tempDir+'/dust-data_');
-    process.on('beforeExit', (code) => {
-      if (!dbPath) return;
-      rimraf(dbPath, {disableGlob: true}, err => {
-        if (err) {
-          console.error(`\r!-> Failed to clean server memory on shutdown: ${err.message}`);
-          console.error(`Temporary data may still exist in`, dbPath);
-        }
-        dbPath = null;
-      });
-      console.log(`Cleaned server memory from disk.`);
-    });
-  }
+  const dbPath = argv.dataPath ||
+    await EstablishTempDir();
 
-  try {
-    fs.accessSync(dbPath, fs.constants.W_OK);
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      console.log('\r--> Creating new data directory', dbPath);
-    } else {
-      throw new Error(`Data path ${dbPath} not writable: ${err.message}`);
-    }
-  }
-
-  console.log('TODO: open database', dbPath);
-  db = { close: () => {} };
-
-  //await db.put('name', 'Level');
-  //console.log('name=' + await db.get('name'));
-
+  db = await ServerDatabase.open(dbPath);
   return db;
 }
+
+//await db.put('name', 'Level');
+//console.log('name=' + await db.get('name'));
