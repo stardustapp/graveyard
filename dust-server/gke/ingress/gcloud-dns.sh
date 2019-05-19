4e7f8423452b8e2d45fd49cb95396b119cb0c2b7
Daniel Lamando <apt@danopia.net>
2019-05-19 15:56:22 -0700

    dust-app: Bind Random package in dust runtime

--- dust-server/gke/ingress/gcloud-dns.sh
+++ dust-server/gke/ingress/gcloud-dns.sh
@@ -3,18 +3,21 @@
 gcloud dns record-sets \
   transaction start \
   --zone=devmodecloud
+
 gcloud dns record-sets \
-  transaction add 104.196.244.167 34.83.242.55 \
+  transaction remove 104.196.244.167 34.83.242.55 35.201.111.60 \
   --name=devmode.cloud. \
   --ttl=300 \
   --type=A \
   --zone=devmodecloud
+
 gcloud dns record-sets \
-  transaction remove 35.201.111.60 \
+  transaction add 34.83.191.127 \
   --name=devmode.cloud. \
   --ttl=300 \
   --type=A \
   --zone=devmodecloud
+
 gcloud dns record-sets \
   transaction execute \
   --zone=devmodecloud
